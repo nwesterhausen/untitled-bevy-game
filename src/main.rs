5b1da046aa871b038388363e9bbe954e88a092b0
Nicholas Westerhausen <nwesterhausen@gmail.com>
2022-11-04 13:07:57 -0400

    feat: ✨ add equi inspector

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,11 @@
 
 use bevy::{
     diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
-    input::mouse::{MouseScrollUnit, MouseWheel},
     log::LogSettings,
     prelude::*,
     winit::WinitSettings,
 };
+use bevy_inspector_egui::{bevy_egui::EguiContext, egui, WorldInspectorPlugin};
 use systems::ingest_file_util::load_ingest_file_dir;
 
 mod systems;
@@ -29,22 +29,28 @@ fn main() {
     // env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
 
     App::new()
+        // add logging, but filter out some noisy logs
         .insert_resource(LogSettings {
             filter:
                 "info,untitled_bevy_game=debug,wgpu_core=warn,wgpu_hal=warn,wgpu_hal::auxil=error"
                     .into(),
             level: bevy::log::Level::DEBUG,
         })
+        // default plugins
         .add_plugins(DefaultPlugins)
+        // fps diagnostic plugin
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        .add_system(text_update_system)
-        .add_system(text_color_system)
-        .add_system(button_system)
+        // egui inspector
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_startup_system(configure_visuals)
         // Only run the app when there is user input. This will significantly reduce CPU/GPU use.
         .insert_resource(WinitSettings::desktop_app())
-        .add_startup_system(setup)
+        // Testing the ingest system is main priority
         .add_startup_system(system_load)
-        .add_system(mouse_scroll)
+        // Load the basic ui system
+        .add_startup_system(basic_ui)
+        // FPS text
+        .add_system(text_update_system)
         .run();
 }
 
@@ -53,104 +59,57 @@ fn system_load() {
     load_ingest_file_dir("ingest_test");
 }
 
-const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
-const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
-
-fn button_system(
-    mut interaction_query: Query<
-        (&Interaction, &mut UiColor, &Children),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut text_query: Query<&mut Text>,
-) {
-    for (interaction, mut color, children) in &mut interaction_query {
-        let mut text = text_query.get_mut(children[0]).unwrap();
-        match *interaction {
-            Interaction::Clicked => {
-                text.sections[0].value = "Press".to_string();
-                *color = PRESSED_BUTTON.into();
-            }
-            Interaction::Hovered => {
-                text.sections[0].value = "Hover".to_string();
-                *color = HOVERED_BUTTON.into();
-            }
-            Interaction::None => {
-                text.sections[0].value = "Button".to_string();
-                *color = NORMAL_BUTTON.into();
-            }
-        }
-    }
+fn configure_visuals(mut egui_ctx: ResMut<EguiContext>) {
+    let mut fonts = egui::FontDefinitions::default();
+    // install your own font (.ttf and .otf supported)
+    fonts.font_data.insert(
+        "agavenf".to_string(),
+        egui::FontData::from_static(include_bytes!(
+            "../assets/fonts/agave regular Nerd Font Complete.ttf"
+        )),
+    );
+    // insert it at the beginning for highest priority
+    fonts
+        .families
+        .get_mut(&egui::FontFamily::Proportional)
+        .unwrap()
+        .insert(0, "agavenf".to_owned());
+    egui_ctx.ctx_mut().set_fonts(fonts);
 }
 
-#[derive(Component, Default)]
-struct ScrollingList {
-    position: f32,
-}
-
-fn mouse_scroll(
-    mut mouse_wheel_events: EventReader<MouseWheel>,
-    mut query_list: Query<(&mut ScrollingList, &mut Style, &Children, &Node)>,
-    query_item: Query<&Node>,
-) {
-    for mouse_wheel_event in mouse_wheel_events.iter() {
-        for (mut scrolling_list, mut style, children, uinode) in &mut query_list {
-            let items_height: f32 = children
-                .iter()
-                .map(|entity| query_item.get(*entity).unwrap().size.y)
-                .sum();
-            let panel_height = uinode.size.y;
-            let max_scroll = (items_height - panel_height).max(0.);
-            let dy = match mouse_wheel_event.unit {
-                MouseScrollUnit::Line => mouse_wheel_event.y * 20.,
-                MouseScrollUnit::Pixel => mouse_wheel_event.y,
-            };
-            scrolling_list.position += dy;
-            scrolling_list.position = scrolling_list.position.clamp(-max_scroll, 0.);
-            style.position.top = Val::Px(scrolling_list.position);
-        }
-    }
-}
-
-// A unit struct to help identify the FPS UI component, since there may be many Text components
-#[derive(Component)]
-struct FpsText;
-
-// A unit struct to help identify the color-changing Text component
-#[derive(Component)]
-struct ColorText;
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // UI camera
+fn basic_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Camera
     commands.spawn_bundle(Camera2dBundle::default());
-    // Text with one section
-    commands
-        .spawn_bundle(
-            // Create a TextBundle that has a Text with a single section.
-            TextBundle::from_section(
-                // Accepts a `String` or any type that converts into a `String`, such as `&str`
-                "hello\nbevy!",
-                TextStyle {
-                    font: asset_server.load("fonts/FredokaOne-Regular.ttf"),
-                    font_size: 100.0,
-                    color: Color::WHITE,
-                },
-            ) // Set the alignment of the Text
-            .with_text_alignment(TextAlignment::TOP_CENTER)
-            // Set the style of the TextBundle itself.
-            .with_style(Style {
-                align_self: AlignSelf::FlexEnd,
-                position_type: PositionType::Absolute,
-                position: UiRect {
-                    bottom: Val::Px(5.0),
-                    right: Val::Px(15.0),
-                    ..default()
-                },
-                ..default()
-            }),
+
+    // See https://github.com/BorisBoutillier/Kataster/blob/main/src/ui.rs for some ideas
+
+    // My custom text to spawn the title
+    commands.spawn_bundle(
+        TextBundle::from_section(
+            "untitled bevy game",
+            TextStyle {
+                font: asset_server.load("fonts/FredokaOne-Regular.ttf"),
+                font_size: 64.0,
+                color: Color::ANTIQUE_WHITE,
+            },
         )
-        .insert(ColorText);
-    // Text with multiple sections
+        .with_text_alignment(TextAlignment {
+            vertical: VerticalAlign::Center,
+            horizontal: HorizontalAlign::Center,
+        })
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                top: Val::Percent(20.0),
+                left: Val::Percent(50.0),
+                ..default()
+            },
+            size: Size::new(Val::Percent(100.0), Val::Px(64.0)),
+            ..default()
+        }),
+    );
+
+    // Text with multiple sections (has the 'FPS' label and the 'FPS' display)
     commands
         .spawn_bundle(
             // Create a TextBundle that has a Text with a list of sections.
@@ -159,13 +118,13 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     "FPS: ",
                     TextStyle {
                         font: asset_server.load("fonts/FredokaOne-Regular.ttf"),
-                        font_size: 60.0,
+                        font_size: 16.0,
                         color: Color::WHITE,
                     },
                 ),
                 TextSection::from_style(TextStyle {
                     font: asset_server.load("fonts/FredokaOne-Regular.ttf"),
-                    font_size: 60.0,
+                    font_size: 16.0,
                     color: Color::GOLD,
                 }),
             ])
@@ -175,46 +134,11 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             }),
         )
         .insert(FpsText);
-    commands
-        .spawn_bundle(ButtonBundle {
-            style: Style {
-                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                // center button
-                margin: UiRect::all(Val::Auto),
-                // horizontally center child text
-                justify_content: JustifyContent::Center,
-                // vertically center child text
-                align_items: AlignItems::Center,
-                ..default()
-            },
-            color: NORMAL_BUTTON.into(),
-            ..default()
-        })
-        .with_children(|parent| {
-            parent.spawn_bundle(TextBundle::from_section(
-                "Button",
-                TextStyle {
-                    font: asset_server.load("fonts/FredokaOne-Regular.ttf"),
-                    font_size: 40.0,
-                    color: Color::rgb(0.9, 0.9, 0.9),
-                },
-            ));
-        });
 }
 
-fn text_color_system(time: Res<Time>, mut query: Query<&mut Text, With<ColorText>>) {
-    for mut text in &mut query {
-        let seconds = time.seconds_since_startup() as f32;
-
-        // Update the color of the first and only section.
-        text.sections[0].style.color = Color::Rgba {
-            red: (1.25 * seconds).sin() / 2.0 + 0.5,
-            green: (0.75 * seconds).sin() / 2.0 + 0.5,
-            blue: (0.50 * seconds).sin() / 2.0 + 0.5,
-            alpha: 1.0,
-        };
-    }
-}
+// A unit struct to help identify the FPS UI component, since there may be many Text components
+#[derive(Component)]
+struct FpsText;
 
 fn text_update_system(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text, With<FpsText>>) {
     for mut text in &mut query {
